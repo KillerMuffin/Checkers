80a169bb275c7dad8caeece0f8c506589f7c221f
Ben McLean <ben@mclean.one>
2016-06-14 11:32:09 +1000

    Fixed broken folder and duplicates

--- Checkers/Checkers/MoveLevel.cpp
+++
@@ -1,7 +0,0 @@
-#include "stdafx.h"
-
-enum MoveLevel{
-	ILLEGAL,
-	LEGAL,
-	CONTINUE
-};
--- Checkers/Checkers/Piece.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "Piece.h"
-
-Piece::Piece(int x, int y, Player * player){
-	this->x = x;
-	this->y = y;
-	this->play = player;
-	this->king = false;
-}
